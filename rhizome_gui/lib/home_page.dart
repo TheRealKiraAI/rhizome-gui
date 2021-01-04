3903e8250a954fb174f96afd70a7d6ab34a62236
Kira Corbett <kiracorbett00@gmail.com>
2021-01-04 11:49:16 -0800

    Experiencing two things visually

--- rhizome_gui/lib/home_page.dart
+++ rhizome_gui/lib/home_page.dart
@@ -19,33 +19,19 @@ class _HomePageState extends State<HomePage> {
     // Now that we have some Things in the Rhizome, let's play with it. 
     // Display all of the information in the world.
     final everything = displayEverything(rhizome);
-
-    // Imagine that the user indicates they wish to focus on one piece of
-    // information. Say, Lewis Hamilton.
-    print('Experiencing. Focusing on Thing: ');
-    final lewisHamilton = everything[2];
-    print(lewisHamilton.information);
-    print('Tags:');
-    final tags = lewisHamilton.tags.map((uri) => rhizome.retrieve(uri));
-    tags.forEach((tag) => print(tag.information));
-    print('Targets:');
-    final targets = lewisHamilton.targets.map((uri) => rhizome.retrieve(uri));
-    targets.forEach((tag) => print(tag.information));
-    
-    print('\n');
     
     // Let's say they want to see if they can view the concept of 'Motorsport'.
     // We can imagine they hit a key, see a text field, and type in 'Motorsport'.
     // It's in the Rhizome, so let's draw our focus on it.
-    print('Experiencing. Focusing on Thing: ');
-    final motorsportThing = rhizome.seek('Motorsport');
-    print(motorsportThing.information);
-    print('Tags:');
-    final motorsportTags = motorsportThing.tags.map((uri) => rhizome.retrieve(uri));
-    motorsportTags.forEach((tag) => print(tag.information));
-    print('Targets:');
-    final motorsportTargets = motorsportThing.targets.map((uri) => rhizome.retrieve(uri));
-    motorsportTargets.forEach((tag) => print(tag.information));
+    // print('Experiencing. Focusing on Thing: ');
+    // final motorsportThing = rhizome.seek('Motorsport');
+    // print(motorsportThing.information);
+    // print('Tags:');
+    // final motorsportTags = motorsportThing.tags.map((uri) => rhizome.retrieve(uri));
+    // motorsportTags.forEach((tag) => print(tag.information));
+    // print('Targets:');
+    // final motorsportTargets = motorsportThing.targets.map((uri) => rhizome.retrieve(uri));
+    // motorsportTargets.forEach((tag) => print(tag.information));
 
     return everything;
   }
@@ -53,33 +39,70 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     List<Thing> everything = _initRhizome();
+    final rhizome = Rhizome();
+
+    // Imagine that the user indicates they wish to focus on one piece of
+    // information. Say, Lewis Hamilton.
+    final lewisHamilton = everything[2];
+    Thing thing1 = experienceThing(lewisHamilton, everything, rhizome);
+
+    final motorsportThing = rhizome.seek('Motorsport');
+    Thing thing2 = experienceThing(motorsportThing, everything, rhizome);
 
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: GridView.count(
-        crossAxisCount: 2,
-        children: List.generate(1, (index) {
-          return Container(
-            child: Card(
-              color: Colors.lightBlueAccent,
-              child: Center(
-                child: visualizeThing(everything),
-              ),
+      body: Column(
+        children: <Widget> [
+          SizedBox(
+            height: 200, // constrain height
+            child: GridView.count(
+              crossAxisCount: 2,
+              children: List.generate(1, (index) {
+                return Container(
+                  child: Card(
+                    color: Colors.lightBlueAccent,
+                    child: Center(
+                      child: visualizeOneThing(thing1),
+                      //child: visualizeThingText(everything),
+                    ),
+                  ),
+                  // child: Text(
+                  //   'Item $index',
+                  //   style: Theme.of(context).textTheme.headline5,
+                  // ),
+                );
+              }),
             ),
-            // child: Text(
-            //   'Item $index',
-            //   style: Theme.of(context).textTheme.headline5,
-            // ),
-          );
-        }),
+          ),
+          SizedBox(
+            height: 200, // constrain height
+            child: GridView.count(
+              crossAxisCount: 2,
+              children: List.generate(1, (index) {
+                return Container(
+                  child: Card(
+                    color: Colors.lightBlueAccent,
+                    child: Center(
+                      child: visualizeOneThing(thing2),
+                      //child: visualizeThingText(everything),
+                    ),
+                  ),
+                  // child: Text(
+                  //   'Item $index',
+                  //   style: Theme.of(context).textTheme.headline5,
+                  // ),
+                );
+              }),
+            ),
+          ),
+        ]
       ),
     );
   }
 
-  Thing experienceThing(Thing thing, List<Thing> everything) {
-    final rhizome = Rhizome();
+  Thing experienceThing(Thing thing, List<Thing> everything, Rhizome rhizome) {
     tagInformation(rhizome);
 
     print('Experiencing. Focusing on Thing: ');
@@ -90,17 +113,15 @@ class _HomePageState extends State<HomePage> {
     print('Targets:');
     final targets = thing.targets.map((uri) => rhizome.retrieve(uri));
     targets.forEach((tag) => print(tag.information));
+    print('\n');
     return thing;
   }
 
-  Widget visualizeThing(List<Thing> everything) {
+  Widget visualizeOneThing(Thing thing) {
     Text textWidget;
 
-    final lewisHamilton = everything[2];
-
     print('========== Experiencing Test ==========');
-    Thing thing1 = experienceThing(lewisHamilton, everything);
-    textWidget = Text('${thing1.information}');
+    textWidget = Text('${thing.information}');
 
     return textWidget;
   }
