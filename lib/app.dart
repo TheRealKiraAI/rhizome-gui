1a789595e9dcabec0745e1c15fffad73eaf6e94f
Yong Bakos <ybakos@humanoriented.com>
2021-01-20 14:10:53 -0800

    App: Replace improper imports.

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:rhizome_gui/home_page.dart';
-import 'package:rhizome_gui/rhizome_gui.dart';
+import 'package:rhizome/rhizome.dart';
+import 'home_page.dart';
 
 class App extends StatelessWidget {
   final Rhizome rhizome;
