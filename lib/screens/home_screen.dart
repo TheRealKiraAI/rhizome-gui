ddc50345fa022eb9b08341c9c55693693f26b5ff
Kira Corbett <kiracorbett00@gmail.com>
2021-01-27 08:12:02 -0800

    HomeScreen: display grid of tiles

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -21,35 +21,12 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      // body: OrientationBuilder(builder: (context, orientation) {
-      //   return GridView.count(
-      //     crossAxisCount: orientation == Orientation.portrait ? 2 : 3,
-      //     children: widget.rhizome.query().map( (thing) => ThingCard(thing: thing, rhizome: widget.rhizome)).toList()
-      //   );
-      // }),
-      body: Stack(
-        children: <Widget>[
-          Positioned(
-            left: offset.dx,
-            top: offset.dy,
-            child: GestureDetector(
-              onPanUpdate: (details) {
-                setState(() {
-                  offset = Offset(offset.dx + details.delta.dx, offset.dy + details.delta.dy);
-                });
-              },
-              child: Container(
-                width: 100, 
-                height: 100, 
-                color: Colors.blue,
-                // how to create draggable box for each map item
-                // children: widget.rhizome.query().map( (thing) => ThingCard(thing: thing, rhizome: widget.rhizome)).toList()
-                child: Text('Thing', textAlign: TextAlign.center),
-              ),
-            ),
-          ),
-        ],
-      )
+      body: OrientationBuilder(builder: (context, orientation) {
+        return GridView.count(
+          crossAxisCount: orientation == Orientation.portrait ? 2 : 3,
+          children: widget.rhizome.query().map( (thing) => ThingCard(thing: thing, rhizome: widget.rhizome)).toList()
+        );
+      }),
     );
   }
 }
