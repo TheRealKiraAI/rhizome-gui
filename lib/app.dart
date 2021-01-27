671e58a061b40cd57526e5e95405190b479c0c97
Yong Bakos <ybakos@humanoriented.com>
2021-01-27 14:17:18 -0800

    App: Set app name.
    Rather than the default, set a simple app name.

--- lib/app.dart
+++ lib/app.dart
@@ -9,7 +9,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Rhizome Shell',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
