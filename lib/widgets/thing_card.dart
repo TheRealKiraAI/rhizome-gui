20e09ae1b2f0de2a0fba19f3e618caea9accbe75
Kira Corbett <kiracorbett00@gmail.com>
2021-02-01 09:18:00 -0800

    WIP: Convert ThingCard to Stateless

--- lib/widgets/thing_card.dart
+++ lib/widgets/thing_card.dart
@@ -3,18 +3,13 @@ import 'package:rhizome/rhizome.dart';
 import 'package:rhizome_gui/models/rhizome_manager.dart';
 import '../screens/thing_card_screen.dart';
 
-class ThingCard extends StatefulWidget {
+class ThingCard extends StatelessWidget {
   ThingCard({this.thing, this.offset});
 
   final Thing thing;
   final Rhizome rhizome = RhizomeManager.getInstance();
   final Offset offset;
 
-  @override
-  _ThingCardState createState() => _ThingCardState();
-}
-
-class _ThingCardState extends State<ThingCard> {
   @override
   Widget build(BuildContext context) {
     return TextButton(
@@ -26,12 +21,12 @@ class _ThingCardState extends State<ThingCard> {
             context,
             MaterialPageRoute(
                 builder: (context) => ThingCardScreen(
-                    thing: widget.thing, rhizome: widget.rhizome)))
+                    thing: thing, rhizome: rhizome)))
       },
       child: Card(
         color: Colors.lightBlueAccent,
         child: Center(
-          child: Text(widget.thing.information),
+          child: Text(thing.information),
         ),
       ),
     );
