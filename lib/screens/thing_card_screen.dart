45d980066e2166f8ce84be56ce2c47bc930cbb63
Kira Corbett <kiracorbett00@gmail.com>
2021-02-02 11:04:56 -0800

    ThingCardScreen: Init layout

--- lib/screens/thing_card_screen.dart
+++ lib/screens/thing_card_screen.dart
@@ -16,13 +16,73 @@ class ThingCardScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(title),
       ),
-      body: Center(
-        child: Container (
-          height: 300,
-          width: 300,
-          child: ThingCard(thing: thing),
-        ),
-      )
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 20.0),
+            height: 200.0,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: <Widget>[
+                Container(
+                  width: 160.0,
+                  color: Colors.red,
+                ),
+                Container(
+                  width: 160.0,
+                  color: Colors.blue,
+                ),
+                Container(
+                  width: 160.0,
+                  color: Colors.green,
+                ),
+                Container(
+                  width: 160.0,
+                  color: Colors.yellow,
+                ),
+                Container(
+                  width: 160.0,
+                  color: Colors.orange,
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: 300,
+            width: 300,
+            child: ThingCard(thing: thing),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 20.0),
+            height: 200.0,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: <Widget>[
+                Container(
+                  width: 160.0,
+                  color: Colors.red,
+                ),
+                Container(
+                  width: 160.0,
+                  color: Colors.blue,
+                ),
+                Container(
+                  width: 160.0,
+                  color: Colors.green,
+                ),
+                Container(
+                  width: 160.0,
+                  color: Colors.yellow,
+                ),
+                Container(
+                  width: 160.0,
+                  color: Colors.orange,
+                ),
+              ],
+            ),
+          ),
+        ]
+      ),
     );
   }
 }
