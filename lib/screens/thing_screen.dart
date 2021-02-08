1826298db4b5141038bf5689990c8fea97d516bf
Kira Corbett <kiracorbett00@gmail.com>
2021-02-08 09:07:57 -0800

    ThingScreen: Moved tags/targets to widget

--- lib/screens/thing_screen.dart
+++ lib/screens/thing_screen.dart
@@ -25,39 +25,35 @@ class ThingScreen extends StatelessWidget {
         title: Text(title),
       ),
       body: Column(children: [
-        Container(
-          margin: EdgeInsets.symmetric(vertical: 20.0),
-          height: 200.0,
-          child: ListView.builder(
-              itemCount: seekingTags.length,
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (BuildContext context, int index) {
-                return _tagAndTargets(seekingTags);
-              }),
-        ),
+        _tagAndTargetList(seekingTags),
         Container(
           color: Colors.purple,
           height: 300,
           width: 300,
           child: ThingCard(thing: thing),
         ),
-        Container(
-          margin: EdgeInsets.symmetric(vertical: 20.0),
-          height: 200.0,
-          child: ListView.builder(
-              itemCount: seekingTargets.length,
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (BuildContext context, int index) {
-                return _tagAndTargets(seekingTargets);
-              }),
-        ),
+        _tagAndTargetList(seekingTargets),
       ]),
     );
   }
 
+  Widget _tagAndTargetList(Iterable<Thing> t) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 20.0),
+      height: 200.0,
+      child: ListView.builder(
+          itemCount: t.length,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (BuildContext context, int index) {
+            print(t.length.toString());
+            print("SeekingTags: " + t.toString());
+            return _tagAndTargets(t);
+          }),
+    );
+  }
+
   Widget _tagAndTargets(Iterable<Thing> t) {
-    return new Row(
-        children: t.map((tagTarget) => ThingCard(thing: tagTarget)).toList());
+    return new Row(children: t.map((tagTarget) => ThingCard(thing: tagTarget)).toList());
   }
 
   void debugPrint(Thing thing, Rhizome rhizome) {
