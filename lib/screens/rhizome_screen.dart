ea36b13693f73f1ffc5a2ad46c92372dcefa2ee5
Kira Corbett <kiracorbett00@gmail.com>
2021-02-08 09:50:18 -0800

    Init zoom

--- lib/screens/rhizome_screen.dart
+++ lib/screens/rhizome_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:zoom_widget/zoom_widget.dart';
 import 'package:rhizome/rhizome.dart';
 import 'package:rhizome_gui/screens/create_information_screen.dart';
 import 'package:rhizome_gui/screens/moveable_screen.dart';
@@ -6,32 +7,32 @@ import '../models/rhizome_manager.dart';
 import '../widgets/thing_card.dart';
 
 class RhizomeScreen extends StatefulWidget {
-
   final Rhizome rhizome = RhizomeManager.getInstance();
-  
+
   RhizomeScreen({Key key}) : super(key: key);
 
   @override
   _RhizomeScreenState createState() => _RhizomeScreenState();
-
 }
 
 class _RhizomeScreenState extends State<RhizomeScreen> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Rhizome'),
-      ),
-      body: OrientationBuilder(builder: (context, orientation) {
-        return GridView.count(
-            crossAxisCount: orientation == Orientation.portrait ? 2 : 3,
-            children: _thingCards(widget.rhizome));
-      }),
-      floatingActionButton: _addButton(),
-      bottomNavigationBar: _bottomNavigationBar()
-    );
+        appBar: AppBar(
+          title: Text('Rhizome'),
+        ),
+        body: Zoom(
+          width: 500,
+          height: 500,
+          child: OrientationBuilder(builder: (context, orientation) {
+            return GridView.count(
+                crossAxisCount: orientation == Orientation.portrait ? 2 : 3,
+                children: _thingCards(widget.rhizome));
+          }),
+        ),
+        floatingActionButton: _addButton(),
+        bottomNavigationBar: _bottomNavigationBar());
   }
 
   List<ThingCard> _thingCards(Rhizome rhizome) {
@@ -58,26 +59,23 @@ class _RhizomeScreenState extends State<RhizomeScreen> {
 
   Widget _bottomNavigationBar() {
     return BottomAppBar(
-        color: Colors.grey[300],
-        child: new Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            IconButton(
-                icon: Icon(Icons.compare_arrows),
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => MoveableScreen()));
-                }),
-            IconButton(
-              icon: Icon(Icons.search),
-              onPressed: () {},
-            ),
-          ],
-        ),
-      );
+      color: Colors.grey[300],
+      child: new Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          IconButton(
+              icon: Icon(Icons.compare_arrows),
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => MoveableScreen()));
+              }),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
   }
-
 }
