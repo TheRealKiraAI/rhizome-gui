453d0618431a57258c9b6aa0005df3456a1203c1
Kira Corbett <kiracorbett00@gmail.com>
2021-02-08 11:53:23 -0800

    Removed prints

--- lib/screens/rhizome_screen.dart
+++ lib/screens/rhizome_screen.dart
@@ -27,18 +27,14 @@ class _RhizomeScreenState extends State<RhizomeScreen> {
         ),
         body: GestureDetector(
           onScaleStart: (ScaleStartDetails details) {
-            print(details);
             _previousScale = _scale;
             setState(() {});
           },
           onScaleUpdate: (ScaleUpdateDetails details) {
-            print(details);
             _scale = _previousScale * details.scale;
             setState(() {});
           },
           onScaleEnd: (ScaleEndDetails details) {
-            print(details);
-
             _previousScale = 1.0;
             setState(() {});
           },
