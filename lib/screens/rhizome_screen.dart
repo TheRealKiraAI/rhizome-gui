c43aa2984e6d7b07cefc2ad6913923c3fbd354c3
Kira Corbett <kiracorbett00@gmail.com>
2021-02-08 11:38:41 -0800

    RhizomeScreen: Panning of grid

--- lib/screens/rhizome_screen.dart
+++ lib/screens/rhizome_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:zoom_widget/zoom_widget.dart';
 import 'package:rhizome/rhizome.dart';
 import 'package:rhizome_gui/screens/create_information_screen.dart';
 import 'package:rhizome_gui/screens/moveable_screen.dart';
+import 'package:vector_math/vector_math_64.dart' show Vector3;
 import '../models/rhizome_manager.dart';
 import '../widgets/thing_card.dart';
 
@@ -16,20 +16,47 @@ class RhizomeScreen extends StatefulWidget {
 }
 
 class _RhizomeScreenState extends State<RhizomeScreen> {
+  double _scale = 1.0;
+  double _previousScale = 1.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('Rhizome'),
         ),
-        body: Zoom(
-          width: 500,
-          height: 500,
-          child: OrientationBuilder(builder: (context, orientation) {
-            return GridView.count(
-                crossAxisCount: orientation == Orientation.portrait ? 2 : 3,
-                children: _thingCards(widget.rhizome));
-          }),
+        body: GestureDetector(
+          onScaleStart: (ScaleStartDetails details) {
+            print(details);
+            _previousScale = _scale;
+            setState(() {});
+          },
+          onScaleUpdate: (ScaleUpdateDetails details) {
+            print(details);
+            _scale = _previousScale * details.scale;
+            setState(() {});
+          },
+          onScaleEnd: (ScaleEndDetails details) {
+            print(details);
+
+            _previousScale = 1.0;
+            setState(() {});
+          },
+          child: RotatedBox(
+            quarterTurns: 0,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Transform(
+                alignment: FractionalOffset.center,
+                transform: Matrix4.diagonal3(Vector3(_scale, _scale, _scale)),
+                child: OrientationBuilder(builder: (context, orientation) {
+                  return GridView.count(
+                      crossAxisCount: orientation == Orientation.portrait ? 2 : 3,
+                      children: _thingCards(widget.rhizome));
+                }),
+              ),
+            ),
+          ),
         ),
         floatingActionButton: _addButton(),
         bottomNavigationBar: _bottomNavigationBar());
