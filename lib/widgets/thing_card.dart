07b498985e549de3f76db7952076950ce6485a19
Kira Corbett <kiracorbett00@gmail.com>
2021-02-10 12:24:47 -0800

    Init drag target

--- lib/widgets/thing_card.dart
+++ lib/widgets/thing_card.dart
@@ -3,7 +3,6 @@ import 'package:rhizome/rhizome.dart';
 import '../screens/thing_screen.dart';
 
 class ThingCard extends StatelessWidget {
-
   final Thing thing;
 
   ThingCard({this.thing});
@@ -15,23 +14,29 @@ class ThingCard extends StatelessWidget {
         foregroundColor: MaterialStateProperty.all(Colors.deepOrange),
       ),
       onPressed: () => {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => ThingScreen(thing: thing))
-          )
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => ThingScreen(thing: thing)))
       },
-      child: Container(
-        width: 150,
-        height: 150,
-        child: Card(
-          color: Colors.lightBlueAccent,
-          child: Center(
-            child: Text(thing.information),
-          ),
-        ),
+      child: DragTarget(
+        builder: (context, List<Thing> candidateData, rejectedData) {
+          return Container(
+            width: 150,
+            height: 150,
+            child: Card(
+              color: Colors.lightBlueAccent,
+              child: Center(
+                child: Text(thing.information),
+              ),
+            ),
+          );
+        },
+        onWillAccept: (data) {
+          return true;
+        },
+        onAccept: (data) {
+          print('accept');
+        },
       ),
     );
   }
-  
 }
