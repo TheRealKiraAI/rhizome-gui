4d6b358c088095f09430100a0612d78ce68f7f4b
Kira Corbett <kiracorbett00@gmail.com>
2021-03-10 09:28:24 -0800

    WIP: Init concept of zooming and displaying a new object

--- lib/widgets/thing_world.dart
+++ lib/widgets/thing_world.dart
@@ -23,6 +23,21 @@ class _ThingWorldState extends State<ThingWorld> {
   String scale = "scale";
   TransformationController controller = TransformationController();
 
+  bool visibilityTag = false;
+  bool visibilityObs = false;
+
+  void _changed(bool visibility, String field) {
+    setState(() {
+      if (field == "tag"){
+        visibilityTag = visibility;
+      }
+      if (field == "obs"){
+        visibilityObs = visibility;
+      }
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -39,6 +54,7 @@ class _ThingWorldState extends State<ThingWorld> {
                   scale = updateDetails.scale.toString();
                   if (updateDetails.scale < 0.5) {
                     scale = "<0.5";
+                    _changed(true, "tag");
                   }
                 });
               },
@@ -51,7 +67,70 @@ class _ThingWorldState extends State<ThingWorld> {
               child: Image.asset('assets/images/moab.jpg')),
         ),
         Text(scale, style: TextStyle(fontWeight: FontWeight.bold)),
-      ]),
+        Container(
+          child: Column(
+            children: <Widget>[
+                visibilityTag ? new Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    new Expanded(
+                      flex: 11,
+                      child: new TextField(
+                        maxLines: 1,
+                        style: Theme.of(context).textTheme.title,
+                        decoration: new InputDecoration(
+                          labelText: "Tags",
+                          isDense: true
+                        ),
+                      ),
+                    ),
+                    new Expanded(
+                      flex: 1,
+                      child: new IconButton(
+                        color: Colors.grey[400],
+                        icon: const Icon(Icons.cancel, size: 22.0,),
+                        onPressed: () {
+                          _changed(false, "tag");
+                        },
+                      ),
+                    ),
+                  ],
+                ) : new Container(),
+              ],
+            )
+          ),
+          new Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new SizedBox(width: 24.0),
+              new InkWell(
+                onTap: () {
+                  visibilityTag ? null : _changed(true, "tag");
+                },
+                child: new Container(
+                  margin: new EdgeInsets.only(top: 16.0),
+                  child: new Column(
+                    children: <Widget>[
+                      new Icon(Icons.local_offer, color: visibilityTag ? Colors.grey[400] : Colors.grey[600]),
+                      new Container(
+                        margin: const EdgeInsets.only(top: 8.0),
+                        child: new Text(
+                          "Tags",
+                          style: new TextStyle(
+                            fontSize: 12.0,
+                            fontWeight: FontWeight.w400,
+                            color: visibilityTag ? Colors.grey[400] : Colors.grey[600],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ),
+            ],
+          )                    
+        ],
+      )
     );
   }
 }
