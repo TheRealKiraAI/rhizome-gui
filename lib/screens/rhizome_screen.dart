d60522cc18c4b44c3d572e2ea66e79853c3768fb
Kira Corbett <kiracorbett00@gmail.com>
2021-03-31 12:15:37 -0700

    Reset init image after zoom

--- lib/screens/rhizome_screen.dart
+++ lib/screens/rhizome_screen.dart
@@ -13,9 +13,12 @@ class RhizomeScreen extends StatefulWidget {
 }
 
 class _RhizomeScreenState extends State<RhizomeScreen> {
+  TransformationController controller = TransformationController();
+
   @override
   Widget build(BuildContext context) {
     final moabImage = widget.rhizome.seek('assets/images/moab.jpg');
+    var initController;
 
     return Scaffold(
       appBar: AppBar(
@@ -27,10 +30,13 @@ class _RhizomeScreenState extends State<RhizomeScreen> {
           boundaryMargin: EdgeInsets.all(double.infinity),
           minScale: 0.1,
           maxScale: 4,
+          transformationController: controller,
+          onInteractionStart: (details) {
+            initController = controller.value;
+          },
           onInteractionUpdate: (ScaleUpdateDetails details) {
             print('Pinch detected: ' + details.scale.toString());
             if (details.scale <= 0.6) {
-              print('<= 0.8');
               setState(() {
                 Navigator.push(
                     context,
@@ -40,16 +46,19 @@ class _RhizomeScreenState extends State<RhizomeScreen> {
               });
             }
           },
+          onInteractionEnd: (ScaleEndDetails details) {
+            controller.value = initController;
+          },
           child: Hero(
             tag: moabImage.information,
             child: Image(
-            image: AssetImage('assets/images/moab.jpg'),
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            fit: BoxFit.fill,
+              image: AssetImage('assets/images/moab.jpg'),
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              fit: BoxFit.fill,
+            ),
           ),
         ),
-      ),
       ),
       floatingActionButton: _addButton(moabImage),
     );
